7d8f7ccaae270f7fa3d64b583be8b5bc79dac02c
vleiswal <vleiswal@gmail.com>
2021-03-26 04:38:44 +0200

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'flavor.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Provider<Flavor>.value(
+    value: Flavor.dev,
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -67,11 +73,14 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final flavor = Provider.of<Flavor>(context);
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          flavor.toString(),
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
